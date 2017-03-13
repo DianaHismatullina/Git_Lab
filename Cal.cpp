554b49039ff783573fcf0179e41d740d7d40acb4
DianaHismatullina <dianarh@rambler.ru>
2017-03-13 13:52:14 +0400

    Create Cal.cpp

---
+++ Cal.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+
+double summa(double a, double b)
+{
+    return  a + b;
+}
+
+
+int main()
+{
+    double a, b, c;
+    printf("\n a = "); scanf("%lf", &a);
+    printf("\n b = "); scanf("%lf", &b);
+    printf("\n summa =%f", summa(a, b));
+    return 0;
+}
